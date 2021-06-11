655fe596518824dd382bf690b8079ea3984ebf7e
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-06-11 16:21:35 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -18,10 +18,24 @@ class Form extends React.Component {
       lastName: event.target.value
     })
   }
+  
+  handleSubmit = event => {
+    event.preventDefault();
+    let formData = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName
+    }
+    
+    let dataArray = this.state.submittedData.concat(formData);
+    this.setState({
+      submittedData: dataArray
+    })
+    }
+  }
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleSubmit}>
         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
