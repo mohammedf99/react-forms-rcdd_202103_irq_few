966c9401c8f848beccb8ec8fcf7f7f6b27cf2db4
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-06-11 16:21:51 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -35,7 +35,7 @@ class Form extends React.Component {
 
   render() {
     return (
-      <form onSubmit={event => this.handleSubmit}>
+      <form onSubmit={event => this.handleSubmit(event)}>
         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
